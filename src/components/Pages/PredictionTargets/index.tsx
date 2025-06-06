b6536381c1d8784b5008425673236c16d24fc21e
hienle-bita <hien_le_vu_toan@bita.jp>
2025-06-06 10:48:19 +0700

    Implement UI for Prediction Targets

--- src/components/Pages/PredictionTargets/index.tsx
+++ src/components/Pages/PredictionTargets/index.tsx
@@ -3,8 +3,17 @@ import type { AdminViewServerProps } from 'payload'
 import { DefaultTemplate } from '@payloadcms/next/templates'
 import { Gutter } from '@payloadcms/ui'
 import React from 'react'
+import { PredictionTargetsAdminContent } from './container/PredictionTargetsAdminContent'
+import { PredictionTargetsPredictorContent } from './container/PredictionTargetsPredictorContent'
+import { User } from '@/payload-types'
+
+interface UserWithRoles extends User {
+  roles?: string[]
+}
 
 const PredictionTarget = ({ initPageResult, params, searchParams }: AdminViewServerProps) => {
+  const isAdmin = (initPageResult.req.user as UserWithRoles)?.roles?.includes('admin')
+
   return (
     <DefaultTemplate
       i18n={initPageResult.req.i18n}
@@ -16,7 +25,9 @@ const PredictionTarget = ({ initPageResult, params, searchParams }: AdminViewSer
       user={initPageResult.req.user || undefined}
       visibleEntities={initPageResult.visibleEntities}
     >
-      <Gutter></Gutter>
+      <Gutter>
+        {!isAdmin ? <PredictionTargetsAdminContent /> : <PredictionTargetsPredictorContent />}
+      </Gutter>
     </DefaultTemplate>
   )
 }
