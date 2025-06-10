d9f26f517c1954c17b2afc62e050a77d2fcc38c5
tunglethanh1998 <tung_le_thanh@bita.jp>
2025-06-10 08:06:40 +0700

    feat: import map

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -6,10 +6,10 @@ import { default as default_82cb54cea17942e21ee8956c92300c78 } from '@/component
 import { default as default_a0a77058b8a476d297b9f7a3fedbf647 } from '@/components/Pages/PredictionTargetsAdmin'
 
 export const importMap = {
-  '@/components/AfterNavLinks#default': default_aa936f6d4bfcfa210342637718f4c629,
-  '@/components/Pages/RaceList#default': default_ad44889f58df4e99210146c51158a6ba,
-  '@/components/Pages/RaceSettings#default': default_d5a1575951a32156bf5f24180b347a19,
-  '@/components/Pages/RaceInfo#default': default_0bbbc96747a3f698ac75221e815b383d,
-  '@/components/Pages/PredictionTargets#default': default_82cb54cea17942e21ee8956c92300c78,
-  '@/components/Pages/PredictionTargetsAdmin#default': default_a0a77058b8a476d297b9f7a3fedbf647,
+  "@/components/AfterNavLinks#default": default_aa936f6d4bfcfa210342637718f4c629,
+  "@/components/Pages/RaceList#default": default_ad44889f58df4e99210146c51158a6ba,
+  "@/components/Pages/RaceSettings#default": default_d5a1575951a32156bf5f24180b347a19,
+  "@/components/Pages/RaceInfo#default": default_0bbbc96747a3f698ac75221e815b383d,
+  "@/components/Pages/PredictionTargets#default": default_82cb54cea17942e21ee8956c92300c78,
+  "@/components/Pages/PredictionTargetsAdmin#default": default_a0a77058b8a476d297b9f7a3fedbf647
 }
