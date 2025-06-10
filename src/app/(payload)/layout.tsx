a8171287beb74f152c14bffbb7361ed7bb768a27
tunglethanh1998 <tung_le_thanh@bita.jp>
2025-06-10 10:38:03 +0700

    feat: change config for docker build

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -7,7 +7,7 @@ import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
-import '@/styles/globals.scss'
+import '../../styles/globals.scss'
 
 type Args = {
   children: React.ReactNode
