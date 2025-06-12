569e891d73446b4a35ef992bd8d5fe7a4cf4d4b9
hienle-bita <hien_le_vu_toan@bita.jp>
2025-06-12 09:13:02 +0700

    Update layouts

--- src/app/(frontend)/123124/123124/1241/page.tsx
+++ src/app/(frontend)/123124/123124/1241/page.tsx
@@ -22,16 +22,16 @@ export const metadata: Metadata = {
 const RenewalInProgressPage = () => {
   return (
     <div
-      className="flex flex-col items-center justify-start min-h-screen bg-white bg-repeat bg-center"
+      className="flex justify-center min-h-screen bg-white bg-repeat bg-center"
       style={{ backgroundImage: "url('/tb-yosou_info-renewal_bg.png')" }}
     >
-      <h1 className="text-center my-8 text-black">
+      <h1 className="text-center my-8 mx-auto text-black">
         <Image
           src="/tb-yosou_info-renewal.png"
           alt="サイトリニューアル中！ 日頃より予想NAVIをご愛顧いただき、誠にありがとうございます！便利で快適なサービスをお届けするため、2025年4月1日より本サイトリニューアルを実施いたします。リニューアル後の便利で快適なサービスを楽しみにお待ちいただけますと幸いです。今後ともどうぞよろしくお願いいたします！"
           width={800}
           height={800}
-          className="w-3/4 max-w-2xl md:w-full"
+          className="w-[90%] inline-block"
           priority
         />
       </h1>
